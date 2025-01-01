3cee7cbbd9080369a0c8d73fc10bf160e521660a
Christopher Kok <ForceLightning@users.noreply.github.com>
2025-01-01 13:22:55 +0800

    refactor(cogs/count): use an sqlite3 db instead of json file handling

--- cogs/count.py
+++ cogs/count.py
@@ -1,9 +1,6 @@
-import json
-import os
+import sqlite3
 import warnings
 from enum import Enum, auto
-from io import TextIOWrapper
-from typing import Any
 
 import discord
 from discord.ext import commands
@@ -50,18 +47,28 @@ class ConfirmationView(discord.ui.View):
     @discord.ui.button(label="Confirm", style=discord.ButtonStyle.primary, emoji="✅")
     async def confirm_callback(self, button, interaction: discord.Interaction):
         self.value = ConfirmDeny.CONFIRM
-        await interaction.response.send_message("Confirming...", ephemeral=True)
+        await interaction.response.send_message(
+            "Confirming...", ephemeral=True, delete_after=15
+        )
 
     @discord.ui.button(label="Deny", style=discord.ButtonStyle.secondary, emoji="❌")
     async def deny_callback(self, button, interaction: discord.Interaction):
         self.value = ConfirmDeny.DENY
-        await interaction.response.send_message("Denying...", ephemeral=True)
+        await interaction.response.send_message(
+            "Denying...", ephemeral=True, delete_after=15
+        )
 
 
 class IncrementButton(discord.ui.Button):
     """A button that can increment/decrement a count for any view."""
 
-    def __init__(self, guild_id: int, message_id: int | None, type: ButtonType):
+    def __init__(
+        self,
+        guild_id: int,
+        message_id: int | None,
+        type: ButtonType,
+        con: sqlite3.Connection,
+    ):
         match type:
             case ButtonType.INCREMENT:
                 style = discord.ButtonStyle.primary
@@ -78,80 +85,71 @@ class IncrementButton(discord.ui.Button):
             emoji=emoji,
             custom_id=f"{guild_id}::{type}",
         )
+        self.con = con
+        self.cur = self.con.cursor()
         self.guild_id = guild_id
         self.message_id: int
         if message_id:
             self.message_id = message_id
         self.button_type = type
-        self.cache: dict[str, Any]
-        if not os.path.exists("cache.json"):
-            with open("cache.json", "w", encoding="utf-8") as f:
-                self.cache = {}
-                json.dump(self.cache, f, sort_keys=True)
-                return
-
-        with open("cache.json", "r+", encoding="utf-8") as f:
-            try:
-                self.cache = json.load(f)
-            except json.JSONDecodeError:
-                self.cache = {}
-                warnings.warn(
-                    "JSON decode failed, silently overwriting to empty dict.",
-                    stacklevel=1,
-                )
-                f.seek(0)
-                f.truncate(0)
-                json.dump(self.cache, f, sort_keys=True)
-
-    async def save_cache(self):
-        with open("cache.json", "r+", encoding="utf-8") as f:
-            f.seek(0)
-            f.truncate(0)
-            json.dump(self.cache, f, sort_keys=True, indent=2)
 
     def post_init_message_id(self, message_id: int):
         self.message_id = message_id
 
     async def callback(self, interaction: discord.Interaction):
-        with open("cache.json", "r", encoding="utf-8") as f:
-            self.cache = json.load(f)
         assert interaction.guild_id is not None
         # NOTE: This button should not even exist if the initial pinned message is not set.
         try:
             assert interaction.channel is not None
             assert isinstance(interaction.channel, discord.TextChannel)
             message = await interaction.channel.fetch_message(self.message_id)
+            update_query = "UPDATE counting SET count = count {} 1, active = TRUE WHERE server_id = ?"
             match self.button_type:
                 case ButtonType.INCREMENT:
-                    self.cache[str(interaction.guild_id)]["count"] += 1
+                    update_query = update_query.format("+")
                 case ButtonType.DECREMENT:
-                    self.cache[str(interaction.guild_id)]["count"] -= 1
+                    update_query = update_query.format("-")
 
-            count = self.cache[str(interaction.guild_id)]["count"]
-            await self.save_cache()
+            self.cur.execute(update_query, (self.guild_id,))
+            select_query = "SELECT count FROM counting WHERE server_id = ?"
+            count, *_ = self.cur.execute(select_query, (self.guild_id,)).fetchone()
             embed = create_count_embed(count)
             await message.edit(embed=embed)
-            await interaction.response.send_message("Count updated.", ephemeral=True)
+            await interaction.response.send_message(
+                "Count updated.", ephemeral=True, delete_after=15
+            )
 
-        except discord.NotFound:
+        except discord.NotFound as e:
             await interaction.response.send_message(
                 "Original pinned message not found. Resetting state.",
                 ephemeral=True,
+                delete_after=15,
             )
-            self.cache[str(interaction.guild_id)] = {}
-            await self.save_cache()
+            self.cur.execute(
+                "UPDATE counting SET active = FALSE WHERE server_id = ?",
+                (self.guild_id,),
+            )
+            warnings.warn(
+                f"{e}: message with id: {self.message_id} not found.", stacklevel=2
+            )
             return
-        except discord.Forbidden:
+        except discord.Forbidden as e:
             await interaction.response.send_message(
                 "Original pinned message is no longer accessible by the bot. Resetting state.",
                 ephemeral=True,
+                delete_after=15,
             )
-            self.cache[str(interaction.guild_id)] = {}
-            await self.save_cache()
+            self.cur.execute(
+                "UPDATE counting SET active = FALSE WHERE server_id = ?",
+                (self.guild_id,),
+            )
+            warnings.warn(str(e), stacklevel=2)
             return
         except discord.HTTPException as e:
             await interaction.response.send_message(
-                f"HTTP error with code: {e.status}. Try again later.", ephemeral=True
+                f"HTTP error with code: {e.status}. Try again later.",
+                ephemeral=True,
+                delete_after=15,
             )
             return
         except Exception as e:
@@ -163,46 +161,23 @@ class Counting(commands.Cog, name="Counting"):
 
     def __init__(self, bot):
         self.bot = bot
-        self.cache: dict[str, Any]
-        if not os.path.exists("cache.json"):
-            with open("cache.json", "w", encoding="utf-8") as f:
-                self.cache = {}
-                json.dump(self.cache, f, sort_keys=True, indent=True)
+        self.con = sqlite3.connect("cache.db")
+        self.cur = self.con.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS counting(server_id INTEGER PRIMARY KEY, message_id INTEGER, count INTEGER, active BOOLEAN NOT NULL CHECK (active IN (0, 1)))"
+        )
 
     @commands.slash_command(description="Initialises the count for the server.")
     async def init_counter(self, ctx: "Context", initial_value: int):
         assert ctx.guild is not None
 
-        # Initialise for the guild and channel if not set.
-        if not os.path.exists("cache.json"):
-            with open("cache.json", "w", encoding="utf-8") as f:
-                embed = create_count_embed(initial_value)
-                message = await ctx.send(embed=embed)
-
-                self.cache = {
-                    str(ctx.guild.id): {
-                        "count": initial_value,
-                        "message_id": message.id,
-                    }
-                }
-
-                json.dump(self.cache, f, sort_keys=True, indent=True)
-
+        res: tuple[int, int, int, bool] | None = self.cur.execute(
+            "SELECT * FROM counting WHERE ? = server_id", (ctx.guild.id,)
+        ).fetchone()
+        if res is None:
+            await self.create_count(ctx, initial_value)
         else:
-            with open("cache.json", "r+", encoding="utf-8") as f:
-                self.cache = json.load(f)
-                if (guild_entry := self.cache.get(str(ctx.guild.id), None)) is not None:
-                    if isinstance(guild_entry, dict):
-                        if (count := guild_entry.get("count", None)) is not None and (
-                            message_id := guild_entry.get("message_id", None)
-                        ) is not None:
-                            await self.handle_override(
-                                ctx, count, message_id, initial_value, f
-                            )
-                            return
-
-                await self.create_count(ctx, initial_value, f)
-                return
+            await self.handle_override(ctx, res[2], res[1], initial_value)
 
     async def handle_override(
         self,
@@ -210,7 +185,6 @@ class Counting(commands.Cog, name="Counting"):
         count: int,
         message_id: int,
         initial_value: int,
-        f: TextIOWrapper,
     ):
         """Handles the case where an original counting message exists and will be overwritten."""
         # GUARD: Check that the original count message can be retrieved.
@@ -234,85 +208,75 @@ class Counting(commands.Cog, name="Counting"):
                 view=None,
             )
 
-            await self.update_count(ctx, initial_value, count_msg, f)
+            await self.update_count(ctx, initial_value, count_msg)
 
             return
-        except (discord.NotFound, discord.Forbidden):
+        except (discord.NotFound, discord.Forbidden) as e:
             await ctx.reply(
                 "The original counting message could not be found. Check the bot's permissions and whether the original message exists. Resetting state."
             )
-            with open("cache.json", "r+", encoding="utf-8") as f:
-                await self.create_count(ctx, initial_value, f)
+            await self.create_count(ctx, initial_value)
+            warnings.warn(
+                f"{e}: Message with id: {message_id} not found or not accessible.",
+                stacklevel=2,
+            )
         except discord.HTTPException as e:
             await ctx.reply(f"HTTP error with code: {e.status}. Try again later.")
             return
 
-    async def update_count(
-        self, ctx: "Context", count: int, count_msg: Message, file: TextIOWrapper
-    ):
+    async def update_count(self, ctx: "Context", count: int, count_msg: Message):
         """Handles the case where the count is simply updated."""
         embed = create_count_embed(count)
         await count_msg.edit(embed=embed)
-        self.cache[str(ctx.guild.id)] = {
-            "count": count,
-            "message_id": count_msg.id,
-        }
+        self.cur.execute(
+            "UPDATE counting SET message_id = ?, count = ?, active = TRUE WHERE server_id = ?",
+            (count_msg.id, count, ctx.guild.id),
+        )
 
-        file.seek(0)
-        file.truncate(0)
-        json.dump(self.cache, file, sort_keys=True, indent=2)
-
-    async def create_count(self, ctx: "Context", count: int, file: TextIOWrapper):
+    async def create_count(self, ctx: "Context", count: int):
         """Handles the case where a new count needs to be created."""
         embed = create_count_embed(count)
         view = discord.ui.View(timeout=None)
-        decrement = IncrementButton(ctx.guild.id, None, ButtonType.DECREMENT)
-        increment = IncrementButton(ctx.guild.id, None, ButtonType.INCREMENT)
+        decrement = IncrementButton(ctx.guild.id, None, ButtonType.DECREMENT, self.con)
+        increment = IncrementButton(ctx.guild.id, None, ButtonType.INCREMENT, self.con)
         view.add_item(decrement)
         view.add_item(increment)
         count_msg = await ctx.reply(embed=embed, view=view)
         decrement.post_init_message_id(count_msg.id)
         increment.post_init_message_id(count_msg.id)
-        self.cache[str(ctx.guild.id)] = {
-            "count": count,
-            "message_id": count_msg.id,
-        }
-
-        file.seek(0)
-        file.truncate(0)
-        json.dump(self.cache, file, sort_keys=True, indent=2)
+        if (
+            self.cur.execute(
+                "SELECT * FROM counting WHERE server_id = ?", (ctx.guild.id,)
+            ).fetchone()
+            is None
+        ):
+            self.cur.execute(
+                "INSERT INTO counting (server_id, message_id, count, active) VALUES (?, ?, ?, TRUE)",
+                (ctx.guild.id, count_msg.id, count),
+            )
+        else:
+            self.cur.execute(
+                "UPDATE counting SET message_id = ?, count = ?, active = TRUE WHERE server_id = ?",
+                (count_msg.id, count, ctx.guild.id),
+            )
 
     @commands.Cog.listener()
     async def on_ready(self):
         """Attach the views to the persistent buttons."""
-        with open("cache.json", "r+", encoding="utf-8") as f:
-            try:
-                self.cache = json.load(f)
-            except json.JSONDecodeError:
-                self.cache = {}
-                warnings.warn(
-                    "JSON decode failed, silently overwriting to empty dict.",
-                    stacklevel=2,
-                )
-                f.seek(0)
-                f.truncate(0)
-                json.dump(self.cache, f, sort_keys=True)
-
-            for guild_id, guild_entries in self.cache.items():
-                view = discord.ui.View(timeout=None)
-                if (
-                    "count" in guild_entries.keys()
-                    and "message_id" in guild_entries.keys()
-                ):
-                    decrement = IncrementButton(
-                        int(guild_id), guild_entries["message_id"], ButtonType.DECREMENT
-                    )
-                    increment = IncrementButton(
-                        int(guild_id), guild_entries["message_id"], ButtonType.INCREMENT
-                    )
-                    view.add_item(decrement)
-                    view.add_item(increment)
-                    self.bot.add_view(view)
+        res: list[tuple[int, int]] = self.cur.execute(
+            "SELECT server_id, message_id FROM counting WHERE active = TRUE"
+        ).fetchall()
+        for server_id, message_id in res:
+            view = discord.ui.View(timeout=None)
+            decrement = IncrementButton(
+                server_id, message_id, ButtonType.DECREMENT, self.con
+            )
+            view.add_item(decrement)
+            increment = IncrementButton(
+                server_id, message_id, ButtonType.INCREMENT, self.con
+            )
+            view.add_item(increment)
+            self.bot.add_view(view)
 
 
 def setup(bot):

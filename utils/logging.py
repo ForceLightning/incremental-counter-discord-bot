ae652e0b6afc29c098f356215260c6f706ed5722
Christopher Kok <ForceLightning@users.noreply.github.com>
2025-07-26 12:13:29 +0800

    fix: logging format $ -> %

--- utils/logging.py
+++ utils/logging.py
@@ -1,3 +1,3 @@
 """Logging utilities."""
 
-LOGGING_FORMAT = "%(asctime)s [%(levelname)s] - $(filename)s:%(lineno)d: %(message)s"
+LOGGING_FORMAT = "%(asctime)s [%(levelname)s] - %(filename)s:%(lineno)d: %(message)s"
